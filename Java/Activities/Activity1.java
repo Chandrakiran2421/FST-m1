d7a32896b34495fe590b23b8a779e4cf9d9da838
Chandrakiran2421 <ambatichandrakiran2421@gmail.com>
2025-07-23 13:24:46 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,19 @@
+package Activities;
+
+public class Activity1 {
+	public static void main(String[] args) {
+	
+				
+		car Kia=new car();
+		 Kia.color= "white";
+		Kia.transformation="Automatic";
+		Kia.make=2025;
+		
+		Kia.displaycharactristics();
+		Kia.accelarate();
+		Kia.brake();
+
+	}
+
+}
+
