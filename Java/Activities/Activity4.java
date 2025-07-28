0145228fb5772f283bf53a0fa20a5bdec801da9d
Chandrakiran2421 <ambatichandrakiran2421@gmail.com>
2025-07-28 13:38:45 +0530

    Add files via upload

---
+++ Java/Activities/Activity4.java
@@ -0,0 +1,17 @@
+package Activities;
+
+import java.util.*;
+
+public class Activity4 {
+	public static void main(String [] args) {
+		int [] my_arr= {34,54,23,67,5,8,90,23,45,12,2,0,26};
+		System.out.println("Initial array is :"+ Arrays.toString(my_arr));
+		
+		Arrays.sort(my_arr);
+		
+		System.out.println("Sorted array is :"+ Arrays.toString(my_arr));
+		
+		
+	}
+
+}
